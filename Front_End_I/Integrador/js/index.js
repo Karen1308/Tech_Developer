de1444dbb04c6b91d1c61306d1a4c26daf07a623
Karen1308 <karengarcia1308@gmail.com>
2022-07-01 01:40:58 -0300

    Terminé con el footer y la parte responsive - Faltan agregar animaciones

--- Front_End_I/Integrador/js/index.js
+++ Front_End_I/Integrador/js/index.js
@@ -2,7 +2,7 @@ const toggleBttn = document.getElementById('toggle-bttn')
 const navBarLink = document.getElementById('nav-bar-link')
 const navSlide = document.getElementById('nav-slide')
 const slide = document.getElementsByClassName("opinion");
-const dots = document.getElementsByClassName("circle");
+const dots = document.getElementsByClassName("dot");
 var slideIndex = 1;
 
 toggleBttn.addEventListener('click', () => {
@@ -11,19 +11,24 @@ toggleBttn.addEventListener('click', () => {
 
 // Funciones para el slide
 // Crear navegacion con los ciculos
-function navigationDots(){
-    for (i = 1; i <= slide.length; i ++){
-        elementBttn = `<button class="circle" onclick="slideNumber(${i})"></button>`;
+function navigationDots() {
+    for (i = 1; i <= slide.length; i++) {
+        elementBttn = `<button class="dot" id="dot${i}" onclick="slideNumber(${i})"></button>`;
         navSlide.innerHTML += elementBttn;
-    }
+
+        if (i ==1){
+            dot1 = document.getElementById('dot'+i);
+            dot1.classList.toggle('dot-active');
+        }
+    };
 };
 
-function slideNumber(number){
+function slideNumber(number) {
     showSlide(slideIndex = number);
 };
 
 // Mostrando slide seleccionado en los botones
-function showSlide(number){
+function showSlide(number) {
     if (number > slide.length) {
         slideIndex = 1
     };
@@ -39,6 +44,14 @@ function showSlide(number){
 
     // Se muestra el slide del boton seleccionado
     slide[slideIndex - 1].style.display = "block";
+
+    // Elimino el que esta activo
+    dotActive = document.getElementsByClassName('dot-active')[0];
+    dotActive.classList.toggle('dot-active');
+
+    // Activamos el seleccionado
+    bttn = document.getElementById('dot' + number);
+    bttn.classList.toggle('dot-active');
 };
 
 // Llamo a la funciona navigation para los puntos
